401e8161c7e1db720ad782c6a02a21269b9e0bce
ankitsinha <ankit.sinha@itaas.com>
2014-07-30 23:31:17 +0530

    Created HTTP Data Provider

--- src/com/itaas/ankit/snippetviewer/HttpDataListProvider.java
+++ src/com/itaas/ankit/snippetviewer/HttpDataListProvider.java
@@ -1,11 +1,83 @@
 package com.itaas.ankit.snippetviewer;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.itaas.ankit.snippetviewer.PojoDataList.PojoSnippet;
+
 public class HttpDataListProvider implements DataListProvider {
+	private static final String TAG = HttpDataListProvider.class.getName();
+
+	public static final String DATA_URI_STRING = "https://www.dropbox.com/s/g41ldl6t0afw9dv/facts.json?dl=1";
+
+	public URI getURI() throws URISyntaxException{
+		return new URI(DATA_URI_STRING);
+	}
 
 	@Override
 	public DataList getDataList() {
-		// TODO Auto-generated method stub
-		return null;
+		String content = fetchDataString();
+		DataList dataList = parseJson(content);
+		return dataList;
+	}
+
+	private String fetchDataString() {
+		String content = null;
+		try {
+			Log.d(TAG, "URI:" + getURI());
+			HttpClient client = new DefaultHttpClient();
+			HttpGet get = new HttpGet(getURI());
+			BasicResponseHandler response = new BasicResponseHandler();
+
+			content = client.execute(get, response);
+			Log.d(TAG, "Got Content" + content.substring(0,100));
+
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Log.d(TAG, "After fetch execution");
+		return content;
+	}
+	
+	public DataList parseJson(String jsonString) {
+		Log.d(TAG, "parseJson");
+
+		GsonBuilder gson = new GsonBuilder();
+		gson.registerTypeAdapter(DataList.Snippet.class, new SnippetDeserializer());
+		
+		DataList dataList = gson.create().fromJson(jsonString, PojoDataList.class);
+		
+		Log.d(TAG, dataList.toString());
+		return dataList;
+	}
+
+	private static class SnippetDeserializer implements JsonDeserializer<DataList.Snippet> {
+		public DataList.Snippet deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+				throws JsonParseException {
+
+			PojoSnippet snippet = context.deserialize(json, PojoSnippet.class);
+			return snippet;
+		}
 	}
 
 }
--- src/com/itaas/ankit/snippetviewer/MainActivity.java
+++ src/com/itaas/ankit/snippetviewer/MainActivity.java
@@ -1,20 +1,11 @@
 package com.itaas.ankit.snippetviewer;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -23,42 +14,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		AsyncTask<URI, Integer, Void> fetchDataTask = new AsyncTask<URI, Integer, Void>(){
+		AsyncTask<Void, Integer, DataList> fetchDataTask = new AsyncTask<Void, Integer, DataList>(){
 
 			@Override
-			protected Void doInBackground(URI... params) {
-				HttpClient client = new DefaultHttpClient();
-				HttpGet get = new HttpGet(params[0]);
-				BasicResponseHandler response = new BasicResponseHandler();
-				String content;
-				try {
+			protected DataList doInBackground(Void... params) {
+				return new HttpDataListProvider().getDataList();
+			}
 
-					Log.d(TAG, "Param:" + params[0]);
-					content = client.execute(get, response);
-					Log.d(TAG, "Got Content" + content.substring(0,100));
-					
-					//Ty
-					
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-					content = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-					content = null;
-				}
-
-				Log.d(TAG, "After fetch execution");
-				return null;
+			@Override
+			protected void onPostExecute(DataList result) {
+				super.onPostExecute(result);
+				Log.d(TAG, "fetchDataTask.onPostExecute");
+				TextView mainTextView = (TextView) MainActivity.this.findViewById(R.id.main_text_view);
+				mainTextView.setText(result.getTitle());
 			}
 
 		};
-		try {
-			fetchDataTask.execute(new URI("https://www.dropbox.com/s/g41ldl6t0afw9dv/facts.json?dl=1"));
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		fetchDataTask.execute();
+		Log.d(TAG, "fetchDataTask.execute triggered");
 	}
 
 	@Override
